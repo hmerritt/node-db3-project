dedb8963a95cd4e63cd5bdd95c3c3a52bc07caef
Harry Merritt <harry@merritt.es>
2020-04-29 01:46:49 +0100

    Answer 2, 3  questions in queries.sql

--- queries.sql
+++ queries.sql
@@ -7,7 +7,17 @@ JOIN [Category]
 WHERE [Product].CategoryId = [Category].Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT [Order].Id, [Shipper].CompanyName
+FROM [Order]
+JOIN [Shipper] ON [Order].ShipVia == [Shipper].id
+WHERE [Order].OrderDate < "2012-08-09";
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT [Product].ProductName, [OrderDetail].Quantity
+FROM [OrderDetail]
+JOIN [Product]
+ON [OrderDetail].ProductId == [Product].Id
+WHERE [OrderDetail].OrderId == "10251"
+ORDER BY [Product].ProductName ASC;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
