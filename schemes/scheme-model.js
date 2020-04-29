351879f7174f609b9ce5977eb04279e84e74a237
Harry Merritt <harry@merritt.es>
2020-04-29 02:37:28 +0100

    Add all db helper functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -6,15 +6,33 @@ function find() {
     return db("schemes");
 }
 
-function findById(id) {}
+function findById(id) {
+    return db("schemes").where("id", id);
+}
 
-function findSteps(id) {}
+function findSteps(id) {
+    return db("schemes")
+        .join("steps", "steps.scheme_id", "schemes.id")
+        .where("schemes.id", id)
+        .select("steps.step_number", "schemes.scheme_name", "steps.instructions")
+        .orderBy("step_number");
+}
 
-function add(scheme) {}
+function add(scheme) {
+    return db("schemes").insert(scheme);
+}
 
-function update(changes, id) {}
+function update(changes, id) {
+    return db("schemes")
+        .where({ id: id })
+        .update(changes);
+}
 
-function remove(id) {}
+function remove(id) {
+    return db("schemes")
+        .where({ id: id })
+        .del();
+}
 
 module.exports = {
     find,
